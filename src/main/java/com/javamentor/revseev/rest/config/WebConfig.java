895af87c795f3d9804a624c8396c474cfad41ecf
revseev <useless47@gmail.com>
2020-01-27 21:51:22 +0300

    not working - before UserDetails fiddling

---
+++ src/main/java/com/javamentor/revseev/rest/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.javamentor.revseev.rest.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+@ComponentScan("com.javamentor.revseev.rest")
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Optional<HttpMessageConverter<?>> converterFound = converters.stream().filter(c -> c instanceof AbstractJackson2HttpMessageConverter).findFirst();
+
+        if (converterFound.isPresent()) {
+            final AbstractJackson2HttpMessageConverter converter =
+                    (AbstractJackson2HttpMessageConverter) converterFound.get();
+            converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+            converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        }
+    }
+}
---
+++ src/main/java/com/javamentor/revseev/rest/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.javamentor.revseev.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @SuppressWarnings("deprecation")
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+            //HTTP Basic authentication
+            .httpBasic()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/**").hasRole("ADMIN")
+            .and()
+            .csrf().disable()
+            .formLogin().disable();
+    }
+
+/*     //для разработки
+   @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring()
+                .antMatchers("/api/**");
+    }*/
+
+/*
+    @Override
+    public void configure(final WebSecurity web) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS);
+    }*/
+
+/* @Bean // CORS Override
+    public FilterRegistrationBean processCorsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("'");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+
+        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }*/
+}
+
